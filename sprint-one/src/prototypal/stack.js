8182160e2dc074012499c98acfbb17101a2702b3
MattRodigheri <mrodigheri@gmail.com>
2018-08-01 16:25:30 -0700

    passed most of prototype stack tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var newStack = Object.create(Stack.prototype);
+  newStack.counter = 0;
+  newStack.storage = {};
+  newStack.storageKeys = Object.keys(newStack.storage);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+Stack.prototype.push = function(value) {
+  this.counter++;
+  this.storageKeys.push(value);
+}
 
+Stack.prototype.pop = function() {
+  this.counter--;
+  if (this.counter < 0) {
+    this.counter = 0;
+  }
+  return this.storageKeys.pop();
+}
+
+Stack.prototype.size = function() {
+  return this.counter;
+}

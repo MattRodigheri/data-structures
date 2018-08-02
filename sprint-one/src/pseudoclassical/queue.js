0eb87555f859202a2c3d604c3aadda79cea3558b
MattRodigheri <mrodigheri@gmail.com>
2018-08-01 17:33:38 -0700

    completed stack and queue for pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.storage = {}
+  this.storageKeys = Object.keys(this.storage);
 };
 
 
+Queue.prototype.enqueue = function(value) {
+  this.counter++;
+  this.storageKeys.push(value);
+}
+
+Queue.prototype.dequeue = function() {
+  this.counter--;
+  if (this.counter < 0) {
+    this.counter = 0;
+  }
+  return this.storageKeys.shift();
+}
+
+Queue.prototype.size = function() {
+  return this.counter;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.storage = {}
+  this.storageKeys = Object.keys(this.storage);
+
 };
 
+Stack.prototype.push = function(value) {
+  this.counter++;
+  this.storageKeys.push(value);
+}
 
+Stack.prototype.pop = function() {
+  this.counter--;
+  if (this.counter < 0) {
+    this.counter = 0;
+  }
+  return this.storageKeys.pop();
+}
+
+Stack.prototype.size = function() {
+  return this.counter;
+}

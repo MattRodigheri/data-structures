217e527395b4009fb7184c4802d8a515b230d54d
MattRodigheri <mrodigheri@gmail.com>
2018-08-05 19:46:12 -0700

    passing tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,72 +8,55 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  //retrieve a bucket
-  var bucket = this._storage.get(index);
-  // if !exist, create it
-  if (!bucket) {
-    bucket = [];
-    this._storage.set(index, bucket);
+    var bucket = this._storage.get(index) || [];
 
-  }
-
-  var found = false;
-  //iterate over bucket
-  for (var i = 0; i = bucket.length; i++) {
-    var tuple = bucket[i];
-    //key exists?
-    if(tuple[0] === k){
-      //update it
-      tuple[1] = v;
-      found = true;
-      break;
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === k) {
+        var oldValue = tuple[1];
+        tuple[1] = v;
+        return oldValue;
+      }
     }
-  }
-  // if no key was found
-  if (!found) {
-    // insert a new tuple
+
     bucket.push([k, v]);
-  }
+    this._storage.set(index, bucket);
+    return undefined;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //retrieve a bucket
-  var bucket = this._storage.get(index);
-  if (!bucket) {
-    return null;
-  }
 
-  //iterate over bucket
-  for (var i = 0; i = bucket.length; i++) {
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i++) {
     var tuple = bucket[i];
-    if(tuple[0] === k ){
+    if (tuple[0] === k) {
       return tuple[1];
     }
   }
-  return null;
+
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  var bucket = this._storage.get(index);
-  if (!bucket) {
-    return null;
-  }
+ var bucket = this._storage.get(index) || [];
 
-  var found = false;
-  //iterate over bucket
-  for (var i = 0; i = bucket.length; i++) {
-    var tuple = bucket[i];
-    //key exists?
-    if(tuple[0] === k ){
-      bucket.splice(i, 1);
-      return tuple[1]
-      break;
-    }
-  }
-  return null;
+ for (var i = 0; i < bucket.length; i++) {
+   var tuple = bucket[i];
+   if (tuple[0] === k) {
+     bucket.splice(i, 1);
+     this._size--;
+     if (this._size < this._limit * 0.25) {
+       this._resize(Math.floor(this._limit / 2));
+     }
+     return tuple[1];
+   }
+ }
+
+ return undefined;
 };
 
 
